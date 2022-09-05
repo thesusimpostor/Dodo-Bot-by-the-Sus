dfa912c401a3d39b59125b2c287d86289a33f707
The Sus <112588540+thesusimpostor@users.noreply.github.com>
2022-09-05 22:16:52 +0200

    Update version.js

--- commands/Other/version.js
+++ commands/Other/version.js
@@ -1,16 +1,9 @@
 module.exports ={
   name:"version",
   aliases:["updates","news","ver"],
-  code: `$title[1;** ⚙️Version: 1.5.9-patched (2022 Aug 26th)**]
+  code: `$title[1;** ⚙️Version: 2.0.0 (2022 sep 5th)**]
 $description[1;
-*NOTE: this release is an patch to fix certain stuff of dodo bot to bring stability*
-
-**bug fixes**
-fixed weather command being returning failed result if user didn't even specify anything such as city
-fixed emojify command being missing of help fun menu cmd
-fix non-existing moderation cmds being listed for some reason
-fixed grammar of mcserver cmd error
-fixed grammar for set-delete-logs cmd
+initial release nothing too much changed just the default used color for embeds of Dodo bot which changed to use aqua color now
 ]
-$color[1;BLURPLE]`
+$color[1;AQUA]`
 } // returns a list of changes in the bot code
